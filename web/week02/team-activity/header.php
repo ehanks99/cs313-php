6acc1aa9503023f6830f2c39a87025caf942a315
Emily Hanks <han17007@byui.edu>
2019-04-30 17:38:00 -0600

    style for nav bar

--- web/week02/team-activity/header.php
+++ web/week02/team-activity/header.php
@@ -4,6 +4,35 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <style>
+        ul 
+        {
+            list-style-type: none;
+            margin: 0;
+            padding: 0;
+            overflow: hidden;
+            background-color: #00004d; /* dark blue */
+        }
+        li 
+        {
+            float: left;
+        }
+
+        li a 
+        {
+            display: block;
+            color: white;
+            text-align: center;
+            padding: 14px 16px;
+            text-decoration: none;
+        }
+
+        li a:hover 
+        {
+            background-color: #005580; /* light-ish blue */
+        }
+    </style>
 </head>
 <body>
 <header>
