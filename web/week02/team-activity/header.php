18267463e45a07a8d9c2fac1b69d98d162a2f10e
Emily Hanks <han17007@byui.edu>
2019-04-30 16:42:00 -0600

    .

--- web/week02/team-activity/header.php
+++ web/week02/team-activity/header.php
@@ -4,10 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Balloon Sales</title>
 </head>
 <body>
-    <input type = "button" value = "Log in as Administrator">
-    <input type = "button" value = "Log in as Tester">
+<header>
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>About Us</li>
+                <li>Login</li>
+            </ul>
+        </nav>
+    </header>
 </body>
 </html>
