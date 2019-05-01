65684e974ecb1ab9f3a020914b79e4c5ad8e110f
Emily Hanks <han17007@byui.edu>
2019-04-30 21:31:39 -0600

    .

--- web/week02/team-activity/about-us.php
+++ web/week02/team-activity/about-us.php
@@ -18,8 +18,10 @@
     </style>
 </head>
 <body>
-    <h2>Our prices are sure to inflate your party! We offer a large selection of 
-    balloons in many different sizes, colors & material. Buy today & save, plus 
-    get free shipping offers on all party decorations!</h2>
+    <div style = "background-color: pink">
+        <h2>Our prices are sure to inflate your party! We offer a large selection of 
+        balloons in many different sizes, colors & material. Buy today & save, plus 
+        get free shipping offers on all party decorations!</h2>
+    </div>
 </body>
 </html>
