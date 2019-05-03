daa68431c47ea5b9438879beeb2dd8b41f828bb4
Emily Hanks <han17007@byui.edu>
2019-05-03 16:50:40 -0600

    .

--- web/homepage/assignments.php
+++ web/homepage/assignments.php
@@ -11,7 +11,7 @@
 <body>
     <div id = "page-container">
         <?php
-            include "navbar.html";
+            include "navbar.php";
         ?>
 
         <div id = "content-wrap">
---
+++ web/homepage/navbar.php
@@ -0,0 +1,7 @@
+<header id = "top">
+    <ul>
+        <li class = "<?php echo $_GET['page'] == "intro.php" ? "active" : ""; ?>" ><a href = "intro.php">Home Page</a></li>
+        <li class = "<?php echo $_GET['page'] == "assignments.php" ? "active" : ""; ?>" ><a href = "assignments.php">Assignments</a></li>
+        <li><a href = "https://byui.instructure.com">Canvas</a></li> 
+    </ul>
+</header>
