3dea2807fe7978e6b6ae0b2689024be8825750f3
Emily Hanks <han17007@byui.edu>
2019-05-03 16:51:17 -0600

    .

--- web/homepage/navbar.php
+++ web/homepage/navbar.php
@@ -1,6 +1,6 @@
 <header id = "top">
     <ul>
-        <li class = "<?php echo $_GET['page'] == "intro.php" ? "active" : ""; ?>" ><a href = "intro.php">Home Page</a></li>
+        <li class = "<?php echo $_GET['page'] == "https://shrouded-scrubland-11365.herokuapp.com/homepage/intro.php" ? "active" : ""; ?>" ><a href = "intro.php">Home Page</a></li>
         <li class = "<?php echo $_GET['page'] == "assignments.php" ? "active" : ""; ?>" ><a href = "assignments.php">Assignments</a></li>
         <li><a href = "https://byui.instructure.com">Canvas</a></li> 
     </ul>
