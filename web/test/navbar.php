eac8b679f1a2b28362d7f72136485724ec1ea9ac
Emily Hanks <han17007@byui.edu>
2019-05-11 16:35:51 -0600

    .

--- web/test/navbar.php
+++ web/test/navbar.php
@@ -1,6 +1,6 @@
 <header id = "top">
     <ul>
-        <li><a href="#" class="btn btn-info btn-lg">
+        <li><a href="#" class="btn btn-default btn-lg">
           <span class="glyphicon glyphicon-shopping-cart"></span> Shopping Cart
         </a></li>
     </ul>
