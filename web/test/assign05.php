7abd4758b8cfc2d69736671068e0ad3e40430bb3
Emily Hanks <han17007@byui.edu>
2019-05-11 18:30:10 -0600

    .

--- web/test/assign05.php
+++ web/test/assign05.php
@@ -66,13 +66,13 @@
    <br/><br/>
    <div class="container-fluid"> <!-- If Needed Left and Right Padding in 'md' and 'lg' screen means use container class -->
             <div class="row">
-                <div class="col-xs-5 col-sm-5 col-md-5 col-lg-5">
+                <div class="col-xs-4">
                 One of three columns
                 </div>
-                <div class="col-xs-5 col-sm-5 col-md-5 col-lg-5">
+                <div class="col-xs-4">
                 One of three columns
                 </div>
-                <div class="col-xs-5 col-sm-5 col-md-5 col-lg-5">
+                <div class="col-xs-4">
                 One of three columns
                 </div>
             </div>
