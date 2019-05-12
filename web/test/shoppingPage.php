1c2c9acda937bb12d4037c47b610c5ed65cb87f1
Emily Hanks <han17007@byui.edu>
2019-05-11 22:40:50 -0600

    .

--- web/test/shoppingPage.php
+++ web/test/shoppingPage.php
@@ -2,9 +2,7 @@
    session_start();
    session_unset();
    $_SESSION["inCart"] = 5;
-   if (!is_writable(session_save_path())) {
-      echo 'Session path "'.session_save_path().'" is not writable for PHP!'; 
-  }
+   echo 'Session path '.session_save_path();
    print_r($_SESSION);
 ?>
 
