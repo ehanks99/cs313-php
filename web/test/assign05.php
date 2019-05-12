3fa8fae237858269475f6cec1226e4ff640309a9
Emily Hanks <han17007@byui.edu>
2019-05-11 18:19:34 -0600

    .

--- web/test/assign05.php
+++ web/test/assign05.php
@@ -63,5 +63,18 @@
             ';
       }
    ?>
+   <div class="container">
+               <div class="row">
+                  <div class="col">
+                     One of three columns
+                  </div>
+                  <div class="col">
+                     One of three columns
+                  </div>
+                  <div class="col">
+                     One of three columns
+                  </div>
+               </div>
+            </div>
 </body>
 </html>
