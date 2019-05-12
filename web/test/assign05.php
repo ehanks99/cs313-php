31c0ae75e741b5cc5dc5eadf8c4549712ed71a19
Emily Hanks <han17007@byui.edu>
2019-05-11 18:22:22 -0600

    .

--- web/test/assign05.php
+++ web/test/assign05.php
@@ -63,18 +63,19 @@
             ';
       }
    ?>
+   <br/><br/>
    <div class="container">
-               <div class="row">
-                  <div class="col col-sm">
-                     One of three columns
-                  </div>
-                  <div class="col col-sm">
-                     One of three columns
-                  </div>
-                  <div class="col col-sm">
-                     One of three columns
-                  </div>
-               </div>
-            </div>
+  <div class="row">
+    <div class="col-sm">
+      One of three columns
+    </div>
+    <div class="col-sm">
+      One of three columns
+    </div>
+    <div class="col-sm">
+      One of three columns
+    </div>
+  </div>
+</div>
 </body>
 </html>
