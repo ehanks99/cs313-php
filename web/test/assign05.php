97f495e6414b02aaacdce8c73aaf1e5f42bad524
Emily Hanks <han17007@byui.edu>
2019-05-11 18:55:36 -0600

    .

--- web/test/assign05.php
+++ web/test/assign05.php
@@ -43,7 +43,7 @@
       $pictureNames = array("Kitten", "Cheeta and Cub", "Chinchilla", "Keychain Animals",
                             "Dog", "Blue Dragon", "White Dragon", "ElephanT", "Narwhal");
       $prices = array("2.00", "10.00", "2.25", "4.00", "4.00", "7.50", "8.00", "12.25", "7.00");
-
+echo $pictures.sizeof();
       for ($i = 0; $i < $pictures.sizeof(); $i++)
       {
          echo '
