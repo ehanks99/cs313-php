155af6f26a6fe8d2b206e1ac7451ff7e7cfa4e54
Emily Hanks <han17007@byui.edu>
2019-05-25 22:40:11 -0600

    .

--- web/project/navbar.php
+++ web/project/navbar.php
@@ -4,7 +4,7 @@
   <a href="#about">Add a Movie</a>
   <a href="#contact">Contact</a>
   <div class="search-container">
-    <form action="/action_page.php">
+    <form action="mainPage.php" method="get">
       <input type="text" placeholder="Search.." name="search">
       <button type="submit"><i class="fa fa-search"></i></button>
     </form>
