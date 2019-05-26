48c757dcbea9d47d1dd5eb409ad1636f7192b8b0
Emily Hanks <han17007@byui.edu>
2019-05-25 20:46:56 -0600

    almost got database call working right

--- web/project/grabData.php
+++ web/project/grabData.php
@@ -49,7 +49,7 @@
                 $stmt = $db->prepare(
                     'SELECT starring_actor.actor_name
                      FROM movie_to_starring_actor
-                        INNER JOIN movie ON movie_to_director.movie_id = movie.movie_id
+                        INNER JOIN movie ON movie_to_starring_actor.movie_id = movie.movie_id
                         INNER JOIN starring_actor ON movie_to_starring_actor.actor_id = starring_actor.actor_id
                      WHERE movie.movie_id = :id');
                 $stmt->execute(array(':id' => $movie_id));
@@ -63,8 +63,8 @@
                 $stmt = $db->prepare(
                     'SELECT genre.genre_type
                      FROM movie_to_genre
-                        INNER JOIN movie ON movie_to_director.movie_id = movie.movie_id
-                        INNER JOIN genre ON movie_genre.genre_id = genre.genre_id
+                        INNER JOIN movie ON movie_to_genre.movie_id = movie.movie_id
+                        INNER JOIN genre ON movie_to_genre.genre_id = genre.genre_id
                         WHERE movie.movie_id = :id');
                 $stmt->execute(array(':id' => $movie_id));
                 while ($row = $stmt->fetch(PDO::FETCH_ASSOC))
