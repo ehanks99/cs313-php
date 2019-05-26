6fdaa380d0d70a86014dbc980cef72a15f3975fa
Emily Hanks <han17007@byui.edu>
2019-05-25 21:46:57 -0600

    .

--- web/project/mainPage.php
+++ web/project/mainPage.php
@@ -27,6 +27,7 @@
                           <p>$' . '$prices[$i]' . '</p><br/>
                           <button type="button" class="btn btn-success">Add to Cart</button>
                        </div>
+                       <br/><hr><br/>
                     </div>
                  </div>
                  <div class="col-md-2"><p></p></div>';
