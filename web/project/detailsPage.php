a3c344bcc7dddffed70a8a86d511be3fd88b7327
Emily Hanks <han17007@byui.edu>
2019-05-26 22:15:16 -0600

    .

--- web/project/detailsPage.php
+++ web/project/detailsPage.php
@@ -27,6 +27,7 @@
         $location = 0;
         for ($i = 0; $i < count($movies); $i++)
         {
+            echo $_POST["movie_name"];
             if (strcmp($movies[$i]["movie_name"], $_POST["movie_name"]))
             {
                 echo 'match on movie name: ' . $movies[$i]["movie_name"];
