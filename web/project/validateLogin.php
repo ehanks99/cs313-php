b5b47025b6d65f8460c1c8eeff97e9c557453c16
Emily Hanks <han17007@byui.edu>
2019-06-01 16:22:56 -0600

    .

--- web/project/validateLogin.php
+++ web/project/validateLogin.php
@@ -12,7 +12,7 @@
     $username = test_input($_POST["username"]);
     $password = test_input($_POST["pswrd"]);
 
-    $stmt = $db->prepare("SELECT username, pswrd, email, first_name, last_name, is_admin FROM login_info WHERE username = ':username';");
+    $stmt = $db->prepare("SELECT username, pswrd, email, first_name, last_name, is_admin FROM login_info WHERE username = ':username'");
     $stmt->execute(array(':username' => $username));
     $row = $stmt->fetchAll(PDO::FETCH_ASSOC);
     
