113cce5744d27de034f13bd5ff74f67bbffa5272
Emily Hanks <han17007@byui.edu>
2019-06-01 15:18:34 -0600

    starting a login page

--- web/project/loginPage.php
+++ web/project/loginPage.php
@@ -12,5 +12,27 @@
 </head>
 <body>
 
+    <div class="container">
+        <h2>Log In</h2>
+        <form class="form-horizontal" action="/action_page.php">
+            <div class="form-group">
+                <label class="control-label col-sm-2" for="username">Username:</label>
+                <div class="col-sm-10">
+                    <input type="text" class="form-control" id="username" placeholder="Enter username" name="username">
+                </div>
+            </div>
+            <div class="form-group">
+                <label class="control-label col-sm-2" for="pwd">Password:</label>
+                <div class="col-sm-10">          
+                    <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pwd">
+                </div>
+            </div>
+            <div class="form-group">        
+                <div class="col-sm-offset-2 col-sm-10">
+                    <button type="submit" class="btn btn-default">Submit</button>
+                </div>
+            </div>
+        </form>
+    </div>
 </body>
 </html>
