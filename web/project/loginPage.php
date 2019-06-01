b56e2f205837db254e6e1da44efe6cc59f8dfaff
Emily Hanks <han17007@byui.edu>
2019-06-01 15:20:29 -0600

    .

--- web/project/loginPage.php
+++ web/project/loginPage.php
@@ -9,6 +9,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Log In</title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+
 </head>
 <body>
 
