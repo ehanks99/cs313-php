15ff890bb7b5242dc27306ced199f4cb1d7020fc
Emily Hanks <han17007@byui.edu>
2019-06-02 16:31:38 -0600

    .

--- web/project/addMovie.php
+++ web/project/addMovie.php
@@ -9,6 +9,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Add a Movie</title>
+
+    <link rel = "stylesheet" type = "text/css" href = "styles.css">
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
 </head>
 <body>
     <?php
