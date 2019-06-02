f9f6dcd5676deb20e34f698bfe2d6879971e0551
Emily Hanks <han17007@byui.edu>
2019-06-01 20:06:30 -0600

    .

--- web/project/loginPage.php
+++ web/project/loginPage.php
@@ -54,7 +54,7 @@
             <div class="form-group">        
                 <div class="col-sm-offset-2 col-sm-10">
                     <button type="submit" class="btn btn-default">Submit</button>
-                    <a href="signUpPage.php">Not a member? Sign up!</a>
+                    &nbsp;&nbsp;&nbsp;&nbsp;<a href="signUpPage.php">Not a member? Sign up!</a>
                 </div>
             </div>
         </form>
