b9e61d9463b888d37b96a1f850bd4916489a996e
Emily Hanks <han17007@byui.edu>
2019-06-02 16:44:29 -0600

    .

--- web/project/addMovie.php
+++ web/project/addMovie.php
@@ -22,5 +22,46 @@
         include 'navbar.php';
     ?>
 
+    
+    <div class="container">
+        <h2 class="text-center">Add a Movie to the List</h2><br/>
+        <form class="form-horizontal" action="validateSignUp.php" method="post">
+            <div class="form-group">
+                <label class="control-label col-sm-2" for="movieName">Movie Name:</label>
+                <div class="col-sm-10">
+                    <input type="text" class="form-control" id="movieName" placeholder="" name="movieName" required>
+                </div>
+            </div>
+            <div class="form-group">
+                <label class="control-label col-sm-2" for="lastName">Last Name:</label>
+                <div class="col-sm-10">
+                    <input type="text" class="form-control" id="lastName" placeholder="Smith" name="lastName" required>
+                </div>
+            </div>
+            <div class="form-group">
+                <label class="control-label col-sm-2" for="username">Username:</label>
+                <div class="col-sm-10">
+                    <input type="text" class="form-control" id="username" placeholder="Enter username" name="username" required>
+                </div>
+            </div>
+            <div class="form-group">
+                <label class="control-label col-sm-2" for="pswrd">Password:</label>
+                <div class="col-sm-10">          
+                    <input type="password" class="form-control" id="pswrd" placeholder="Enter password" name="pswrd" required>
+                </div>
+            </div>
+            <div class="form-group">
+                <label class="control-label col-sm-2" for="email">Email:</label>
+                <div class="col-sm-10">
+                    <input type="email" class="form-control" id="email" placeholder="example@gmail.com" name="email" required>
+                </div>
+            </div>
+            <div class="form-group">        
+                <div class="col-sm-offset-2 col-sm-10">
+                    <button type="submit" class="btn btn-default">Submit</button>
+                </div>
+            </div>
+        </form>
+    </div>
 </body>
 </html>
