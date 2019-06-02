46d92ae7defb6b1a1a1c310ec06bb82616b1b728
Emily Hanks <han17007@byui.edu>
2019-06-01 19:56:12 -0600

    .

--- web/project/loginPage.php
+++ web/project/loginPage.php
@@ -15,12 +15,11 @@
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
 
-<!-- jQuery library -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
-
-<!-- Latest compiled JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script> 
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script> 
 
 </head>
 <body>
@@ -31,7 +30,7 @@
         {
             echo '
             <div class="alert alert-danger text-center">
-              <strong>ERROR:</strong>' . $_GET["error"] . '
+              <strong>ERROR: </strong>' . $_GET["error"] . '
             </div>';
         }
     ?>
--- web/project/navbar.php
+++ web/project/navbar.php
@@ -1,14 +1,14 @@
 
 <div class="topnav">
   <a class="active" href="mainPage.php">Home Page</a>
-  <a href="">Add a Movie</a>
   <?php
     if (isset($_SESSION["loggedIn"]) && $_SESSION["loggedIn"] == true)
     {
       if ($_SESSION["isAdmin"] == "T")
         echo '<a href="addMovie.php">Add Movie</a>';
 
-      echo '<a href="loginPage.php">Hello, ' . $_SESSION["firstName"] . '</a>';
+      //echo '<a href="loginPage.php">Hello, ' . $_SESSION["firstName"] . '</a>';
+      echo '<a href="logout.php">Logout</a>';
     }
     else
       echo '<a href="loginPage.php">Login</a>';
