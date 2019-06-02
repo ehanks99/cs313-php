9ced681c08f0529671ac1f36ea79f9d8feface03
Emily Hanks <han17007@byui.edu>
2019-06-01 19:50:05 -0600

    .

--- web/project/loginPage.php
+++ web/project/loginPage.php
@@ -16,6 +16,12 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script> 
+
 </head>
 <body>
     <?php
@@ -23,8 +29,9 @@
 
         if (isset($_GET["error"]))
         {
-            echo '<div class="alert alert-secondary" role="alert">
-                ERROR: ' . $_POST["error"] . '
+            echo '
+            <div class="alert alert-danger">
+              <strong>ERROR:</strong>' . $_POST["error"] . '
             </div>';
         }
     ?>
