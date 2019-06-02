bec12773421178e8f37b24bd747beff4f248f90c
Emily Hanks <han17007@byui.edu>
2019-06-01 21:24:15 -0600

    .

--- web/project/validateSignUp.php
+++ web/project/validateSignUp.php
@@ -23,13 +23,13 @@
     // if the statement returns something, that means the username is already in use
     if (empty($rows))
     {
-        echo empty($rows);
+        /*echo empty($rows);
         foreach($rows as $row)
         {
             echo $row["email"];
-        }
+        }*/
 
-        /*$stmt = $db->prepare("INSERT INTO login_info (login_info_id, username, pswrd, email, first_name, last_name, is_admin)
+        $stmt = $db->prepare("INSERT INTO login_info (login_info_id, username, pswrd, email, first_name, last_name, is_admin)
                                   VALUES (nextval('login_info_s1'), :user, :pswrd, :email, :firstN, :lastN, 'N')");
         $stmt->execute(array(':user' => $username, ':pswrd' => $password, ':email' => $email, ':firstN' => $firstName, ':lastN' => $lastName));
         
