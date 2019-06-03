865e17a9925413c7bc65ac766b84a15027f35daf
Emily Hanks <han17007@byui.edu>
2019-06-02 21:32:32 -0600

    success with getting the 'add movie' page working

--- web/project/detailsPage.php
+++ web/project/detailsPage.php
@@ -16,6 +16,12 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script> 
+
     <?php
         include 'grabData.php';
     ?>
