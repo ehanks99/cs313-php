a4c02056593af0509147746d1847b1b38714c12c
Emily Hanks <han17007@byui.edu>
2019-06-04 15:40:14 -0600

    .

--- web/week07/team-activity/validateSignUp.php
+++ web/week07/team-activity/validateSignUp.php
@@ -21,7 +21,6 @@
     $stmt->execute();
     $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-    
     // if the statement returns something, that means the username is already in use
     if (empty($rows))
     {
