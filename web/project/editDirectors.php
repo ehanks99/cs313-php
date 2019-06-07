651dc8146fa79108db3f3337a6821b61cf76b335
Emily Hanks <han17007@byui.edu>
2019-06-07 17:18:53 -0600

    .

--- web/project/editDirectors.php
+++ web/project/editDirectors.php
@@ -75,11 +75,8 @@
                 {
                     echo '
                     <div>
-                        <div class="col-sm-3"></div>
-                        <div class="col-sm-6">
                             <input type="text" class="form-control" id="director' . $i . '" value="' . $directors[$i]["director_name"] . '" onkeydown="return false;">
-                        </div>
-                        <div class="col-sm-3">
+
                             <button class="pull-left" style="font-size:20px; margin-bottom:50px;" onclick="editDirector(\'' . $directors[$i]["director_id"] . '\')"><i class="fa fa-edit"></i></button>
                         </div>
                     </div>';
--- web/project/navbar.php
+++ web/project/navbar.php
@@ -1,24 +1,3 @@
-<script>
-  /* When the user clicks on the button, 
-  toggle between hiding and showing the dropdown content */
-  function myFunction() 
-  {
-    document.getElementById("myDropdown").classList.toggle("show");
-  }
-
-  // Close the dropdown if the user clicks outside of it
-  window.onclick = function(e) 
-  {
-    if (!e.target.matches('.dropbtn')) 
-    {
-      var myDropdown = document.getElementById("myDropdown");
-      if (myDropdown.classList.contains('show')) 
-      {
-        myDropdown.classList.remove('show');
-      }
-    }
-  }
-</script>
 
 <div class="topnav">
   <a class="active" href="mainPage.php">Home Page</a>
@@ -35,37 +14,10 @@
     else
       echo '<a href="loginPage.php">Login</a>';
   ?>
-  
-  <div class="dropdown">
-    <button class="dropbtn" onclick="myFunction()">Edit
-      <i class="fa fa-caret-down"></i>
-    </button>
-    <div class="dropdown-content" id="myDropdown">
-      <a href="editDirectors.php">Edit Directors</a>
-      <a href="editActors.php">Edit Actors</a>
-      <a href="#">Link 3</a>
-    </div>
-  </div> 
   <div class="search-container">
     <form action="mainPage.php" method="get">
       <input type="text" placeholder="Search..." name="search">
       <button type="submit"><i class="fa fa-search"></i></button>
     </form>
   </div>
-</div>
-
-
-<div class="navbar">
-  <a href="#home">Home</a>
-  <a href="#news">News</a>
-  <div class="dropdown">
-  <button class="dropbtn" onclick="myFunction()">Dropdown
-    <i class="fa fa-caret-down"></i>
-  </button>
-  <div class="dropdown-content" id="myDropdown">
-    <a href="#">Link 1</a>
-    <a href="#">Link 2</a>
-    <a href="#">Link 3</a>
-  </div>
-  </div> 
 </div>
