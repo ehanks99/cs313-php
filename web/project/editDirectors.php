110a6fac159b57b4ce77e00ec5c514238e60a249
Emily Hanks <han17007@byui.edu>
2019-06-07 15:38:06 -0600

    .

--- web/project/editDirectors.php
+++ web/project/editDirectors.php
@@ -44,14 +44,16 @@
                 {
                     echo '
                     <div>
-                        <div class="col-sm-3"></div>
+                        <div class="col-sm-2"></div>
                         <div class="col-sm-6">
                             <input type="text" class="form-control" id="director' . $i . '" value="' . $directors[$i]["director_name"] . '" disabled>
+                            <button class = "pull-right" style="font-size:20px; margin-bottom:50px;"><i class="fa fa-edit"></i></button>
                         </div>
-                        <div class="col-sm-3"></div>
+                        <div class="col-sm-2"></div>
                     </div>';
                     $i++;
                 }
+                // onclick="editMovie(\'' . $rows['movie_name'] . '\')"
 
 
                 //<div class="col-sm-10">
