0618381f76ce98937e420ec68b65231418cb0cdf
Emily Hanks <han17007@byui.edu>
2019-06-07 00:12:52 -0600

    .

--- web/project/movieInputOptions.php
+++ web/project/movieInputOptions.php
@@ -53,14 +53,14 @@
 
                 echo '</div>';
             ?>
-
-            <!-- add a hidden textfield for the movie id to be placed -->
-            <input type='text' style='visibility:hidden' id='movieId' name='movieId'>
-
             <div class="form-group">        
                 <div class="col-sm-offset-2 col-sm-10">
                     <button type="submit" class="btn btn-default">Submit</button>
                 </div>
             </div>
+
+            <!-- add a hidden textfield for the movie id to be placed -->
+            <input type='text' style='visibility:hidden' id='movieId' name='movieId'>
+
         </form>
     </div>
