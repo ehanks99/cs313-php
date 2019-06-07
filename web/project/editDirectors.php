2ce2e2a3f9b738f3a286810acd666065f960d7bf
Emily Hanks <han17007@byui.edu>
2019-06-07 15:42:27 -0600

    .

--- web/project/editDirectors.php
+++ web/project/editDirectors.php
@@ -42,7 +42,7 @@
                 {
                     echo '
                     <div>
-                        <button class = "pull-right" style="font-size:20px; margin-bottom:50px;"><i class="fa fa-edit"></i></button>
+                        <button style="font-size:20px; margin-bottom:50px;"><i class="fa fa-edit"></i></button>
                         <div class="col-sm-6">
                             <input type="text" class="form-control" id="director' . $i . '" value="' . $directors[$i]["director_name"] . '" disabled>
                         </div>
