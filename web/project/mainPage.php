cb5fcb08d3452ddff8489d08a1c5e9eea33b42c1
Emily Hanks <han17007@byui.edu>
2019-06-07 19:45:42 -0600

    .

--- web/project/mainPage.php
+++ web/project/mainPage.php
@@ -16,8 +16,7 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
-    <!-- jQuery library -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+
     <?php
         include 'grabData.php';
 
