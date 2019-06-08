8f336b2004b3cd3f3f57588fe99ca6862fe3d4e3
Emily Hanks <han17007@byui.edu>
2019-06-07 20:28:17 -0600

    got the navbar working!!

--- web/project/navbar.php
+++ web/project/navbar.php
@@ -24,7 +24,17 @@
     else
       echo '<a href="loginPage.php">Login</a>';
   ?>
-  
+  <div class="myDropdown">
+            <button class="myDropbtn">Edit 
+              <i class="fa fa-caret-down"></i>
+            </button>
+          <div class="myDropdown-content">
+            <a href="editMovieNames.php">Movie Names</a>
+            <a href="editDirectors.php">Directors</a>
+            <a href="editActors.php">Actors</a>
+          </div>
+        </div>
+        
   <div class="search-container">
     <form id="navbarSearch" action="mainPage.php" method="get">
       <input type="text" placeholder="Search..." name="search">
