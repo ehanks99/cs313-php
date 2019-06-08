acbdd43746a37d3f54fd3b7df41fba84f628fac5
Emily Hanks <han17007@byui.edu>
2019-06-07 19:46:30 -0600

    .

--- web/project/mainPage.php
+++ web/project/mainPage.php
@@ -13,7 +13,6 @@
     <link rel = "stylesheet" type = "text/css" href = "new.css">
 
     <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
 
