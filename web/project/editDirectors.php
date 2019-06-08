72adbdc62753bbb7c9e390e16d1f7c4eac480615
Emily Hanks <han17007@byui.edu>
2019-06-07 20:36:24 -0600

    got the navbar working!!

--- web/project/editDirectors.php
+++ web/project/editDirectors.php
@@ -96,7 +96,8 @@
 
         echo '
         <div class="container">
-            <h2 id="heading" class="text-center">Edit Director Names</h2><br/>
+            <h2 id="heading" class="text-center">Edit Director Names</h2>
+            <button type="button" class="btn btn-secondary">Secondary</button><br/>
             ';
                 $i = 0;
                 while ($i < sizeof($directors))
