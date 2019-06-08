24bd0777f8759c8fb6a5881673cfba2dbdb93569
Emily Hanks <han17007@byui.edu>
2019-06-07 19:44:46 -0600

    .

--- web/project/mainPage.php
+++ web/project/mainPage.php
@@ -18,10 +18,6 @@
 
     <!-- jQuery library -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
-
-    <!-- Latest compiled JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script> 
-
     <?php
         include 'grabData.php';
 
