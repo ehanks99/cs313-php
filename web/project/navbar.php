9e83e768069f22328fda422925bda77156b53fe0
Emily Hanks <han17007@byui.edu>
2019-06-07 19:00:46 -0600

    .

--- web/project/navbar.php
+++ web/project/navbar.php
@@ -1,7 +1,19 @@
 
 <div class="navbar">
   <a class="active" href="mainPage.php">Home Page</a>
-  <a href="editMovie.php">Edit Movie</a>
+  <?php
+    if (isset($_SESSION["loggedIn"]) && $_SESSION["loggedIn"] == true)
+    {
+      if ($_SESSION["isAdmin"] == "T")
+        echo '<a href="addMovie.php">Add Movie</a>';
+
+      //echo '<a href="loginPage.php">Hello, ' . $_SESSION["firstName"] . '</a>';
+      echo '<a href="logout.php">Logout</a>';
+    }
+    else
+      echo '<a href="loginPage.php">Login</a>';
+  ?>
+
   <div class="dropdown">
     <button class="dropbtn">Edit 
       <i class="fa fa-caret-down"></i>
