8f3237aecb4ffdae37ccc34426b8a7856d26431a
Emily Hanks <han17007@byui.edu>
2019-06-08 01:42:26 -0600

    .

--- web/project/editMovie.php
+++ web/project/editMovie.php
@@ -125,6 +125,7 @@
             addElement('actors', 'p', 'actor' + actorId, html);
             var tmp = "actor" + actorId;
             document.getElementById(tmp).value = name;
+            alert(document.getElementById(tmp));
         }
 
         function removeElement(elementId) 
